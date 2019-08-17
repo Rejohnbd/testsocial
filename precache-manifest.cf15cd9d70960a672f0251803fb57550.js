dbebc37fb6c30b6021f006b74f27fa3ebff63ada
Rejohnbd <rejohn.cse@gmail.com>
2019-08-18 00:38:34 +0600

    Updates

---
+++ precache-manifest.cf15cd9d70960a672f0251803fb57550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4843bbd85ec687a2702fc2b215abfd1f",
+    "url": "/testsocial/index.html"
+  },
+  {
+    "revision": "433f25beacf6bfe05e22",
+    "url": "/testsocial/static/css/main.0b711f9c.chunk.css"
+  },
+  {
+    "revision": "efaa2f153cedc0aaa813",
+    "url": "/testsocial/static/js/2.0c54e608.chunk.js"
+  },
+  {
+    "revision": "433f25beacf6bfe05e22",
+    "url": "/testsocial/static/js/main.00b17869.chunk.js"
+  },
+  {
+    "revision": "7b4788c55eabb27cec00",
+    "url": "/testsocial/static/js/runtime~main.4eb21c5d.js"
+  },
+  {
+    "revision": "8e5ac0eaca35d68cea2ec70b6ac74d35",
+    "url": "/testsocial/static/media/no-img.8e5ac0ea.jpg"
+  }
+]);
